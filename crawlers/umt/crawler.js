f4a4792170b2d1481f6bd0ab2b29b0bf09dcd531
Unknown <adam.prescott01@googlemail.com>
2021-01-10 01:04:16 +0000

    Updated UMT Crawler to support new sitemap
    Also added .idea to git ignore

--- crawlers/umt/crawler.js
+++ crawlers/umt/crawler.js
@@ -3,27 +3,32 @@ const BaseSitemapCrawler = require('../BaseSitemapCrawler');
 class UMTCrawler extends BaseSitemapCrawler{
     constructor() {
         super({
-            sitemap: 'https://www.unmannedtechshop.co.uk/image-sitemap-index-1.xml'
+            sitemap: 'https://www.unmannedtechshop.co.uk/sitemap.xml'
         });
     }
 
     async fetchUrls() {
         const sitemap = await this._startCrawl(this.sitemap);
 
-        let imagesetPromises = sitemap.sitemapindex.sitemap.map((url) => {
-            return this._fetchProductsFromImageset(url.loc);
-        });
+        let productPostPromises = sitemap.sitemapindex.sitemap
+            .filter(url => {
+                const regex = /^https:\/\/.*\/wp-sitemap-posts-product-[0-9]*.xml$/g
+                return url.loc.match(regex) !== null;
+            })
+            .map((url) => {
+                return this._fetchProductsFromPosts(url.loc);
+            });
 
-        let products = await Promise.all(imagesetPromises);
+        let products = await Promise.all(productPostPromises);
 
         return [...new Set(products.flat())];
     }
 
-    async _fetchProductsFromImageset(sitemapUrl) {
+    async _fetchProductsFromPosts(sitemapUrl) {
         const sitemap = await this._startCrawl(sitemapUrl);
 
         /**
-         * Grab full imagesitemap.
+         * Grab full posts sitemap.
          * Spread into Set to make unique.
          * filter for product only urls.
          */
